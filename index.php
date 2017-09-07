55133070ae47c5a93659c62c7cff6517ea5e13bb
NutHubs <31301535+NutHubs@users.noreply.github.com>
2017-09-07 12:22:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ if (strpos($_msg, 'สอนบอท') !== false) {
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = "NSET Actual : ".number_format($strActual);
+    $arrPostData['messages'][0]['text'] = "NSET Actual : ".number_format($strActual)." unit.";
   }
   else if(strtoupper($_msg) == "AIR1")
   {   
