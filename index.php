07a288d1fdffb155f736c13ff3b89faac9509f1d
NutHubs <31301535+NutHubs@users.noreply.github.com>
2017-09-19 11:27:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -273,8 +273,8 @@ if (strpos($_msg, 'สอนบอท') !== false) {
     	$arrPostData = array();
     	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     	$arrPostData['messages'][0]['type'] = "image";
-    	$arrPostData['messages'][0]['originalContentUrl'] = "http://vtown.vn/uploads/company/9/05082013/thumbnail/Nidec%20Servo%20Vietnam.jpg";
-	$arrPostData['messages'][0]['previewImageUrl'] = "http://vtown.vn/uploads/company/9/05082013/thumbnail/Nidec%20Servo%20Vietnam.jpg";
+    	$arrPostData['messages'][0]['originalContentUrl'] = "https://www.partsit.com/img/manufacturers/nidec.png";
+	$arrPostData['messages'][0]['previewImageUrl'] = "https://www.partsit.com/img/manufacturers/nidec.png";
 	  
   }
   else
