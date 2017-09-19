10f3072da3cda753b2a409e0a31f6b6a75b0e9d4
NutHubs <31301535+NutHubs@users.noreply.github.com>
2017-09-19 15:52:34 +0700

    Update showimage.php

--- showimage.php
+++ showimage.php
@@ -1,13 +1,8 @@
 <?php
-// open the file in a binary mode
-$name = 'http://223.27.205.134:12000/emp_image/070001.jpg';
-$fp = fopen($name, 'rb');
+$im = imagecreatefrompng("http://223.27.205.134:12000/emp_image/070001.jpg");
 
-// send the right headers
-header("Content-Type: image/jpg");
-header("Content-Length: " . filesize($name));
+header('Content-Type: image/png');
 
-// dump the picture and stop the script
-fpassthru($fp);
-exit;
+imagepng($im);
+imagedestroy($im);
 ?>
