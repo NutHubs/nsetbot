89c466e5eeb74d830d50a79b9ce06874d18ba30d
NutHubs <31301535+NutHubs@users.noreply.github.com>
2017-09-19 15:45:20 +0700

    Update showimage.php

--- showimage.php
+++ showimage.php
@@ -1,7 +1,3 @@
 <?php
-    $file = 'http://223.27.205.134:12000/emp_image/070001.jpg';
-
-    header('Content-Type: image/jpeg');
-    header('Content-Length: ' . filesize($file));
-    echo file_get_contents($file);
+    echo "hello world";
 ?>
