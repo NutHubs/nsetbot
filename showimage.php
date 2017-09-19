dfadc0d7784284d0605854386fbc382924a1e270
NutHubs <31301535+NutHubs@users.noreply.github.com>
2017-09-19 15:50:57 +0700

    Update showimage.php

--- showimage.php
+++ showimage.php
@@ -1,3 +1,13 @@
 <?php
-    echo "hello world";
+// open the file in a binary mode
+$name = 'http://223.27.205.134:12000/emp_image/070001.jpg';
+$fp = fopen($name, 'rb');
+
+// send the right headers
+header("Content-Type: image/jpg");
+header("Content-Length: " . filesize($name));
+
+// dump the picture and stop the script
+fpassthru($fp);
+exit;
 ?>
