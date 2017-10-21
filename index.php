9242b799d5cbeea6f121a75d4bf61ff03ad09917
NutHubs <31301535+NutHubs@users.noreply.github.com>
2017-10-21 16:58:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if (strpos($_msg, 'สอนบอท') !== false) {
     $url = "https://api.netpie.io/feed/aircond011withfeed007?apikey=oSHt1BDhi5VLw9nMaRGWcNp02uAXjJQu&granularity=15second&since=1day";
     $response = file_get_contents($url);
     $obj = json_decode($response, true);
-    $strTemp = $obj[0]['lastest_data'];
+    $strTemp = $obj['lastest_data'];
     //$arrTemp = explode("|", $strTemp);
     
     $arrPostData = array();
