0532d33bec5b6bef6dbb4a0a264c4d3cd9b972a1
NutHubs <31301535+NutHubs@users.noreply.github.com>
2017-11-01 16:27:18 +0700

    Create mlab.php

---
+++ mlab.php
@@ -0,0 +1,17 @@
+<?php
+  $mid = "777";
+  $api_key="_ZLyBX6InXGzrE-ki01xKzo-QyXHOwPN";
+  $url = 'https://api.mlab.com/api/1/databases/nsetbot_db/collections/linebot?apiKey='.$api_key.'&q={"mid":"'.$mid'"}';
+  
+  $opts = array('http' =>
+    array(
+      'method' => 'DELETE',
+      'header' => 'Content-type: application/json'
+    ) 
+  );
+  
+  $context = stream_context_create($opts);
+  $returnVal = file_get_content($url, false, $context);
+  echo "Deleted : ".returnVal;
+
+?>
